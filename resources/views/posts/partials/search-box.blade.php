af58f1b78d648f91159f740775f0916c0239be39
PAPATIRI <syariftaufik11@gmail.com>
2024-01-21 14:44:56 +0700

    mengganti search box dengan alpine js agar tidak mengirim network request 2 kali setiap melakukan pencarian ketika menggunakan livewire

--- resources/views/posts/partials/search-box.blade.php
+++ resources/views/posts/partials/search-box.blade.php
@@ -1,4 +1,4 @@
-<div id="search-box">
+<div x-data="{query: '{{request('search', '')}}'}" id="search-box">
     <div>
         <h3 class="text-lg font-semibold text-gray-900 mb-3">Cari artikel</h3>
         <div class="w-fit flex rounded-xl bg-gray-100 py-2 px-3 mb-3 items-center">
@@ -9,9 +9,9 @@
                                       d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.19a7.5 7.5 0 0010.607 10.607z"/>
                             </svg>
                         </span>
-            <input wire:model="search" type="text" placeholder="cari artikel"
+            <input x-model="query" type="text" placeholder="cari artikel"
                    class="w-40 ml-1 bg-transparent focus:outline-none focus:border-none focus:ring-0 outline-none border-none text-sm text-gray-800 placeholder:text-gray-400">
-            <x-button wire:click="searchPost">Cari</x-button>
+            <x-button x-on:click="$dispatch('search', {search:query})" type="submit">Cari</x-button>
         </div>
     </div>
 </div>
