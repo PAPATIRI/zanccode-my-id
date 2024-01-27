de554db46c659eb1174263c8a290d2b74d4c3045
PAPATIRI <syariftaufik11@gmail.com>
2024-01-27 12:35:05 +0700

    menambahkan authorization based on user role di admin panel, dan menambahkan policy pada category user dan post model

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,11 @@
-@props(['active'])
+@props(['active', 'navigate'])
 
 @php
-$classes = ($active ?? false)
-            ? 'flex space-x-2 items-center hover:text-yellow-900 text-yellow-500'
-            : 'flex space-x-2 items-center hover:text-gray-900 text-gray-500';
+    $classes = ($active ?? false)
+                ? 'flex space-x-2 items-center hover:text-yellow-900 text-yellow-500'
+                : 'flex space-x-2 items-center hover:text-gray-900 text-gray-500';
 @endphp
 
-<a wire:navigate {{ $attributes->merge(['class' => $classes]) }}>
+<a {{$navigate ?? true ? 'wire:navigate' : '' }} {{ $attributes->merge(['class' => $classes]) }}>
     {{ $slot }}
 </a>
