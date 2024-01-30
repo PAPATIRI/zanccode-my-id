10cc551345e670a2df258a0075283bc71363e697
PAPATIRI <syariftaufik11@gmail.com>
2024-01-30 23:07:31 +0700

    menambahkan caching dengan file pada homepage dan blog page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -12,9 +14,16 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        $feturedPost = Cache::remember('featuredPost', Carbon::now()->addDays(3), function () {
+            return Post::published()->featured()->with('categories')->latest('published_at')->take(3)->get();
+        });
+        $latestPost = Cache::remember('latestPost', Carbon::now()->addDays(3), function () {
+            return Post::published()->with('categories')->latest('published_at')->take(6)->get();
+        });
+
         return view('home', [
-            'featuredPost' => Post::published()->featured()->with('categories')->latest('published_at')->take(3)->get(),
-            'latestPost' => Post::published()->with('categories')->latest('published_at')->take(6)->get(),
+            'featuredPost' => $feturedPost,
+            'latestPost' => $latestPost
         ]);
     }
 }
