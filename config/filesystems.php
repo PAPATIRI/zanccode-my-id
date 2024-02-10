a48277748ddc82383c0358653b37596cf4d21f66
PAPATIRI <syariftaufik11@gmail.com>
2024-02-10 16:08:24 +0700

    menambah asset url di .env dan opsi setting url fileysystem config untuk production dan menambahkan build folder ke repo git agar tidak perlu melakukan npm run build di local dan baru upload ke cpanel, itu repot sekali beybehh

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,8 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
+            //'url' => env('APP_URL').'/public/storage', //for production setting
             'visibility' => 'public',
             'throw' => false,
         ],
